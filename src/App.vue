20d0cb0e002e51f124624e3a9dee2a76114a39fb
Marcelo Martins <marcelomartins@gmail.com>
2019-08-24 22:37:11 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Bem vindo ao Vue.js App"/>
+  </div>
+</template>
+
+<script>
+    import {Component, Vue} from 'vue-property-decorator';
+    import HelloWorld from './components/HelloWorld';
+
+    @Component({
+        components: {
+            HelloWorld,
+        }
+    })
+    export default class App extends Vue {
+
+        created() {
+            console.log("## Criou APP");
+        }
+
+        mounted() {
+            console.log("## Mountou APP");
+        }
+
+    }
+</script>
+
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
